15929c6588e5216058d39a6791b13dba03494008
Juan Jose Garay Cuellar <jgaray2223@cue.edu.co>
2024-11-10 18:16:52 -0500

    build: inicializa el proyecto y configura el git

---
+++ src/main/java/com/example/proyecto_sgp/ProyectoSgpApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_sgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSgpApplication.class, args);
+	}
+
+}
